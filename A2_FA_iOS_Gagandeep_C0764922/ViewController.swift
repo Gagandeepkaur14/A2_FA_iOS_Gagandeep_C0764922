f0855204d3d6dad790165f70774a9f1338e03415
Gagandeepkaur14 <77646825+Gagandeepkaur14@users.noreply.github.com>
2021-02-01 00:24:50 -0500

    Initial Commit

---
+++ A2_FA_iOS_Gagandeep_C0764922/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_iOS_Gagandeep_C0764922
+//
+//  Created by parneet sandhu on 2021-02-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
